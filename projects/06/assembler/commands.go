dc1c59b12a8a2144284209409a6a9ed379d58f18
tatsuya4559 <tatsuya.k1029@gmail.com>
2024-01-03 23:42:13 +0900

    refactor: split file

---
+++ projects/06/assembler/commands.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type CommandType int
+
+const (
+	A_COMMAND CommandType = iota
+	C_COMMAND
+	L_COMMAND
+)
+
+type Command interface {
+	Type() CommandType
+	String() string
+}
+
+type ACommand struct {
+	Symbol string
+}
+
+func (c *ACommand) Type() CommandType {
+	return A_COMMAND
+}
+
+func (c *ACommand) String() string {
+	return fmt.Sprintf("@%s", c.Symbol)
+}
+
+type CCommand struct {
+	Dest string
+	Comp string
+	Jump string
+}
+
+func (c *CCommand) Type() CommandType {
+	return C_COMMAND
+}
+
+func (c *CCommand) String() string {
+	return fmt.Sprintf("%s=%s;%s", c.Dest, c.Comp, c.Jump)
+}
+
+type LCommand struct {
+	Symbol string
+}
+
+func (c *LCommand) Type() CommandType {
+	return L_COMMAND
+}
+
+func (c *LCommand) String() string {
+	return fmt.Sprintf("(%s)", c.Symbol)
+}
--- projects/06/assembler/parser.go
+++ projects/06/assembler/parser.go
@@ -9,57 +9,6 @@ import (
 	"strings"
 )
 
-type CommandType int
-
-const (
-	A_COMMAND CommandType = iota
-	C_COMMAND
-	L_COMMAND
-)
-
-type Command interface {
-	Type() CommandType
-	String() string
-}
-
-type ACommand struct {
-	Symbol string
-}
-
-func (c *ACommand) Type() CommandType {
-	return A_COMMAND
-}
-
-func (c *ACommand) String() string {
-	return fmt.Sprintf("@%s", c.Symbol)
-}
-
-type CCommand struct {
-	Dest string
-	Comp string
-	Jump string
-}
-
-func (c *CCommand) Type() CommandType {
-	return C_COMMAND
-}
-
-func (c *CCommand) String() string {
-	return fmt.Sprintf("%s=%s;%s", c.Dest, c.Comp, c.Jump)
-}
-
-type LCommand struct {
-	Symbol string
-}
-
-func (c *LCommand) Type() CommandType {
-	return L_COMMAND
-}
-
-func (c *LCommand) String() string {
-	return fmt.Sprintf("(%s)", c.Symbol)
-}
-
 type Parser struct {
 	scanner        *bufio.Scanner
 	buffer         *strings.Builder
